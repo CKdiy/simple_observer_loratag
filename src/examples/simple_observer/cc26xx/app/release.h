eca8cf21887ff1d60bb908918dc4056635c6c815
chenkai <1490520478@qq.com>
2020-08-17 14:13:52 +0800

    版本 V0.0.6

--- src/examples/simple_observer/cc26xx/app/release.h
+++ src/examples/simple_observer/cc26xx/app/release.h
@@ -12,7 +12,7 @@
 #define SW_VER_MINOR            0
 
 /* for release code, patch must be 0 */
-#define SW_VER_PATCH            5
+#define SW_VER_PATCH            6
 
 
 #define SW_BUILD_STR            __TIME__" "__DATE__
